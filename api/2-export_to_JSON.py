1275cfef25035f635f75ba11ac23f789f146cf54
misaelbazan <misaelbfranco@gmail.com>
2024-05-27 16:47:16 -0500

    2nd task file

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""This module contains the script for the 0th task"""
+
+import json
+import requests
+import sys
+
+
+def list_completed_tasks(todo_data):
+    """Count n of completed tasks"""
+    return [task for task in todo_data if task["completed"]]
+
+
+def get_todo_list_titles(todo_data):
+    return [task['title'] for task in todo_data]
+
+
+def todo_list_task_status(todo_data):
+    return [task['completed'] for task in todo_data]
+
+
+def save_to_csv(filename, employee_todos):
+    with open(filename, mode="w", newline="") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerows(employee_todos)
+
+
+def save_to_json(filename, employee_todos):
+    with open(filename, mode="w", newline="") as f:
+        json.dump(employee_todos, f, indent=4)
+
+
+def main():
+    id = sys.argv[1]
+    try:
+        # Fetching user data
+        user_url = f"https://jsonplaceholder.typicode.com/users/{id}"
+        user_response = requests.get(user_url)
+        user_data = user_response.json()
+
+        # Fetching todos data
+        todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={id}"
+        todos_response = requests.get(todos_url)
+        todos_data = todos_response.json()
+
+        EMPLOYEE_NAME = user_data['username']
+        completed_tasks = list_completed_tasks(todos_data)
+        NUMBER_OF_DONE_TASKS = len(completed_tasks)
+        TOTAL_NUMBER_OF_TASKS = len(todos_data)
+
+        todos_list_status = todo_list_task_status(todos_data)
+        todos_titles = get_todo_list_titles(todos_data)
+        
+        employee_todos = {}
+        todos_list = []
+
+        for index in range(0, TOTAL_NUMBER_OF_TASKS):
+            dict_todos = {
+                'task': todos_titles[index],
+                'completed': todos_list_status[index],
+                'username': EMPLOYEE_NAME
+            }
+            todos_list.append(dict_todos)
+        employee_todos = {str(id): todos_list}
+
+        filename = f"{id}.json"
+
+        save_to_json(filename, employee_todos)
+
+    except requests.RequestException as e:
+        print("Error happened during execution", e)
+
+
+if __name__ == "__main__":
+    main()
