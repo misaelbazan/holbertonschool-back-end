758b7a45050de373fa6b254d18afce5411388e6f
misaelbazan <misaelbfranco@gmail.com>
2024-05-27 12:52:36 -0500

    adding module documentation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,28 +4,29 @@
 import requests
 import sys
 
+    
 def list_completed_tasks(todo_data):
-  """Count n of completed tasks"""
-  return [task for task in todo_data if task["completed"]]
+    """Count n of completed tasks"""
+    return [task for task in todo_data if task["completed"]]
 
 try:
-  employee_id = sys.argv[1]
-  # Fetching user data
-  user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-  user_response = requests.get(user_url)
-  user_data = user_response.json()
+    employee_id = sys.argv[1]
+    # Fetching user data
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
 
-  # Fetching todos data
-  todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-  todos_response = requests.get(todos_url)
-  todos_data = todos_response.json()
+    # Fetching todos data
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
     
-  completed_tasks = list_completed_tasks(todos_data)
-  number_completed_tasks = len(completed_tasks)
-  total_tasks = len(todos_data)
+    completed_tasks = list_completed_tasks(todos_data)
+    number_completed_tasks = len(completed_tasks)
+    total_tasks = len(todos_data)
 
-  print(f"Employee {user_data['name']} is done with tasks({number_completed_tasks}/{total_tasks}):")
-  for task in completed_tasks:
-    print(f"\t{task['title']}") 
+    print(f"Employee {user_data['name']} is done with tasks({number_completed_tasks}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t{task['title']}") 
 except:
-  print("Error happened during execution")
+    print("Error happened during execution")
